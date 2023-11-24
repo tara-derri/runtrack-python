6108286df2c9c74c52705e802b8de66a6d3e44d0
tara-derri <tara.derri@laplateforme.io>
2023-11-24 15:46:32 +0100

    corrrection

---
+++ jour01/plus_loin/chaine.py
@@ -0,0 +1,39 @@
+def contient_e(chaine):
+    return 'e' in chaine.lower()
+
+exemple1 = "Téléphone"
+exemple2 = "Ordinateur"
+exemple3 = "Cascade"
+exemple4 = "Cactus"
+exemple5 = "Livre"
+exemple6 = "Montagne"
+
+if contient_e(exemple1):
+    print(f'La chaîne "{exemple1}" contient le caractère "e".')
+else:
+    print(f'La chaîne "{exemple1}" ne contient pas le caractère "e".')
+
+if contient_e(exemple2):
+    print(f'La chaîne "{exemple2}" contient le caractère "e".')
+else:
+    print(f'La chaîne "{exemple2}" ne contient pas le caractère "e".')
+
+if contient_e(exemple3):
+    print(f'La chaîne "{exemple3}" contient le caractère "e".')
+else:
+    print(f'La chaîne "{exemple3}" ne contient pas le caractère "e".')
+
+if contient_e(exemple4):
+    print(f'La chaîne "{exemple4}" contient le caractère "e".')
+else:
+    print(f'La chaîne "{exemple4}" ne contient pas le caractère "e".')
+
+if contient_e(exemple5):
+    print(f'La chaîne "{exemple5}" contient le caractère "e".')
+else:
+    print(f'La chaîne "{exemple5}" ne contient pas le caractère "e".')
+
+if contient_e(exemple6):
+    print(f'La chaîne "{exemple6}" contient le caractère "e".')
+else:
+    print(f'La chaîne "{exemple6}" ne contient pas le caractère "e".')
